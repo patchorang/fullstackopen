f3cf382498edfe797dd09abdb5be4b6d9e163672
Adam Menz <mz.adamQ@gmail.com>
2024-12-06 05:02:12 -0700

    part1 exercises

---
+++ part1/part1/src/App.jsx
@@ -0,0 +1,53 @@
+const Part = (props) => {
+  return (
+    <div>
+      {props.name} {props.exercises}
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <p>
+      <h1>{props.title}</h1>
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      <Part name={props.title} exercises={props.exercises} />
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      <p>Number of exercises {props.total}</p>
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content title={part1} exercises={exercises1} />
+      <Content title={part2} exercises={exercises2} />
+      <Content title={part3} exercises={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
